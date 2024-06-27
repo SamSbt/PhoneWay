547aa2b565f2668c990fb36509cc58efcc148b3e
Sam Sabat <samnsabat@gmail.com>
2024-06-27 11:04:33 +0200

    import bundle (popperjs&bootstrap)

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,21 +8,18 @@
   <link rel="stylesheet" href="style.css">
   <title>PHONE WAY</title>
 </head>
+
 <body>
-  
 
 
-<?php
 
-require __DIR__ . "../src/includes/navbar.php";
+  <?php
 
-?>
+  require __DIR__ . "../src/includes/navbar.php";
 
-<script src="node_modules\bootstrap\dist\js\bootstrap.min.js"></script>
+  ?>
+
+  <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
-
-
-
-
-
